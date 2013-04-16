31dcd6d1d98a7780e3196df88e3f9d99f450749e
Pablo Molina <pablo.molinacandel@gmail.com>
2013-04-16 09:11:22 +0200

    fix async

--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -2,11 +2,14 @@ require 'mechanize'
 
 class Scrapper
   include Sidekiq::Worker
+  sidekiq_options retry: false
 
   def perform
-    Alert.where(sent: false).all.each.hit_web do |alert|
-      AlertMailer.notify(alert).deliver
-      alert.update_attribute(:sent, true)
+    Alert.where(sent: false).each do |alert|
+      alert.hit_web do |a|
+        AlertMailer.notify(a).deliver
+        a.update_attribute(:sent, true)
+      end
     end
   end
 
