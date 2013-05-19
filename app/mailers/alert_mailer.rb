2e3833ac2a5270fe1c88e849e5e8c44c55c121af
Pablo Molina <pablo.molinacandel@gmail.com>
2013-05-19 19:47:17 +0200

    wording

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -6,7 +6,7 @@ class AlertMailer < ActionMailer::Base
 
     mail to: @alert.recipients,
          bcc: "pablo.molinacandel@gmail.com",
-         subject: "¡Tachán! Ya hay trenes disponibles"
+         subject: "¡Tachán! Hay trenes disponibles"
   end
 
   def created(alert)
